75f1e71cc240181120f490413a97c6ee32ebae71
vaibhavtade02 <dummy@gmail.com>
2026-04-08 04:45:50 +0000

    cors

--- server/src/main/java/com/edutech/config/Configurations.java
+++ server/src/main/java/com/edutech/config/Configurations.java
@@ -17,4 +17,17 @@ public class Configurations {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*");
+            }
+        };
+    }
+
 }
